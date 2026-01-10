4484de01172e93051c13d2444b95947197b8183f
TheRealNightmare <nnahid929@gmail.com>
2026-01-10 10:27:30 +0600

    authentication

--- job-portal-backend/app/Models/Application.php
+++ job-portal-backend/app/Models/Application.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'job_id',
+        'seeker_id',
+        'cover_letter',
+        'resume_path',
+        'match_percentage',
+        'status',
+    ];
+
+    public function job()
+    {
+        return $this->belongsTo((job::class));
+    }
+
+    public function seeker()
+    {
+        return $this->belongsTo(User::class, 'seeker_id');
+    }
 }
