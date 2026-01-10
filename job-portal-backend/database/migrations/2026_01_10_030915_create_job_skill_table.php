afda7c345e233bd2a7af65e3b6392474a51c7ea3
TheRealNightmare <nnahid929@gmail.com>
2026-01-10 09:13:54 +0600

    job & skills migrations

--- job-portal-backend/database/migrations/2026_01_10_030915_create_job_skill_table.php
+++ job-portal-backend/database/migrations/2026_01_10_030915_create_job_skill_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('job_skill', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('job_id')->constrained()->onDelete('cascade');
+            $table->foreignId('skill_id')->constrained()->onDelete('cascade');
         });
     }
 
--- job-portal-backend/database/migrations/2026_01_10_030926_create_user_skill_table.php
+++ job-portal-backend/database/migrations/2026_01_10_030926_create_user_skill_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_skill', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('skill_id')->constrained()->onDelete('cascade');
         });
     }
 
