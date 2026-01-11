93cd2caa76a37c2d7b9efedf8e41ab9b803e04ca
TheRealNightmare <nnahid929@gmail.com>
2026-01-11 22:14:06 +0600

    EmployerController methods updated and routes added

---
+++ job-portal-backend/app/Http/Controllers/Api/EmployerController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Job;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class EmployerController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'skills' => 'required|array'
+        ]);
+
+        $job = Job::create([
+            'employer_id' => auth('sanctum')->id(),
+            'title' => $request->title,
+            'description' => $request->description,
+            'location' => $request->location,
+            'salary_range' => $request->salary,
+            'type' => $request->type()
+        ]);
+
+        foreach($request->skills as $skillName) {
+            $skill = Skill::firstOrCreate(['name' => trim($skillName)]);
+            $job->skills()->attach($skill->id);
+        }
+        return response()->json($job);
+    }
+
+    public function getApplicats($jobId) {
+        $job = Job::where('id', $jobId)->where('employer_id', auth('sanctum')->id())->firstOrFail();
+        $applicants = $job->applications()
+           ->with('seeker.skills')
+           ->orderBy('match_percentage', 'desc')
+           ->get();
+        return response()->json($applicants);
+    }
+}
--- job-portal-backend/routes/api.php
+++ job-portal-backend/routes/api.php
@@ -1,13 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\ApplicationController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EmployerController;
+use App\Http\Controllers\Api\JobController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/jobs', [JobController::class, 'index']); // Public search
+Route::get('/jobs/{id}', [JobController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout',[AuthController::class, 'logout']);
+    Route::post('/jobs/{id}/apply', [ApplicationController::class, 'apply']);
+    Route::get('/my-applications', [ApplicationController::class, 'myApplications']);
+    Route::post('/employer/jobs', [EmployerController::class, 'store']);
+    Route::get('/employer/jobs', [EmployerController::class, 'myJobs']);
+    Route::get('/employer/jobs/{id}/applicants', [EmployerController::class, 'getApplicants']);
 });
