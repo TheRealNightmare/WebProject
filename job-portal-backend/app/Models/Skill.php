95472325aef0ab7ffa87858a9726cfaee48c1bc4
TheRealNightmare <nnahid929@gmail.com>
2026-01-13 12:39:34 +0600

    skill model updated

--- job-portal-backend/app/Models/Skill.php
+++ job-portal-backend/app/Models/Skill.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Skill extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = ['name'];
+
+    public function jobs()
+    {
+        return $this->belongsToMany(Job::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
