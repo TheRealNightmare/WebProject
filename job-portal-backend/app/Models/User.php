74288baf6d9d8e029795e92cbf4a778beb6e7d2b
TheRealNightmare <nnahid929@gmail.com>
2026-01-13 12:50:36 +0600

    user and skill relation fixes

--- job-portal-backend/app/Models/User.php
+++ job-portal-backend/app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
 
     public function skills()
     {
-        return $this->belongsToMany(Skill::class);
+        return $this->belongsToMany(Skill::class, 'user_skill');
     }
     public function applications()
     {
